bee47f8b7e461879ed4939a84421afe5c317a1bf
eestelle <yosiky@list.ru>
2022-09-17 19:50:50 +0300

    day00: ex00: ...

--- day00/ex00/hh.sh
+++ day00/ex00/hh.sh
@@ -1,7 +1,8 @@
 #!/bin/shell
 
 vacancy=$(echo $1 | sed 's/ /%20/g')
-echo $vacancy
-curl -k -H 'User-Agent: api-test-agent' "https://api.hh.ru/vacancies?text=$vacancy" 1> hh_copy.json
+echo "You want to search : $vacancy"
+echo
+curl  "https://api.hh.ru/vacancies?text=$vacancy&page=0&per_page=20" 1> hh_copy.json
 cat hh_copy.json | jq '.' 1> hh.json
 rm hh_copy.json
