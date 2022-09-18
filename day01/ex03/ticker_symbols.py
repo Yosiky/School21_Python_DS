a6c29e73b9bfb1d012b39a66062c4471a9a34d81
eestelle <yosiky@list.ru>
2022-09-18 16:33:02 +0300

    day01: ex03: finish

---
+++ day01/ex03/ticker_symbols.py
@@ -0,0 +1,30 @@
+import sys
+
+def find_price_company(arg):
+    COMPANIES = {
+    'Apple': 'AAPL',
+    'Microsoft': 'MSFT',
+    'Netflix': 'NFLX',
+    'Tesla': 'TSLA',
+    'Nokia': 'NOK'
+    }
+    STOCKS = {
+    'AAPL': 287.73,
+    'MSFT': 173.79,
+    'NFLX': 416.90,
+    'TSLA': 724.88,
+    'NOK': 3.37
+    }
+    arg = arg.strip().upper()
+    LIST = list(COMPANIES.values())
+    if arg in LIST:
+        indx = LIST.index(arg)
+        print(list(COMPANIES.keys())[indx], STOCKS[arg])
+    else:
+        print("Unknown company")
+
+if __name__ == '__main__':
+    count = len(sys.argv)
+    if count == 2:
+        find_price_company(sys.argv[1])
+
