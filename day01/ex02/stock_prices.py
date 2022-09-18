bbb2a6d0901bde0c6b856b0e5cc20caefd62ebd4
eestelle <yosiky@list.ru>
2022-09-18 16:13:35 +0300

    day01: ex02: finish

---
+++ day01/ex02/stock_prices.py
@@ -0,0 +1,28 @@
+import sys
+
+def find_price_company(arg):
+    COMPANIES = {
+    'Apple': 'AAPL',
+    'Microsoft': 'MSFT',
+    'Netflix': 'NFLX',
+    'Tesla': 'TSLA',
+    'Nokia': 'NOK'
+    }
+    STOCKS = {
+    'AAPL': 287.73,
+    'MSFT': 173.79,
+    'NFLX': 416.90,
+    'TSLA': 724.88,
+    'NOK': 3.37
+    }
+    arg = arg[0].upper() + arg[1:].lower()
+    if arg in COMPANIES:
+        print(STOCKS[COMPANIES[arg]])
+    else:
+        print("Unknown company")
+
+if __name__ == '__main__':
+    count = len(sys.argv)
+    if count == 2:
+        find_price_company(sys.argv[1])
+
