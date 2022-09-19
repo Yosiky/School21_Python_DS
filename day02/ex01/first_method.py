e05825a4cc8ca51e01cbb70174da8892354e1689
eestelle <yosiky@list.ru>
2022-09-19 20:51:56 +0300

    day02: ex01: fixed

--- day02/ex01/first_method.py
+++ day02/ex01/first_method.py
@@ -1,12 +1,12 @@
 
 class Research:
-    def file_reader():
+    def file_reader(self):
         with open('data.csv', 'r') as file:
             for i in file.readlines():
                 print(i, end='')
 
 
 if __name__ == '__main__':
-    a = Research
+    a = Research()
     a.file_reader()
 
