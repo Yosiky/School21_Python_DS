d9c71cb590f4516fb64eba7b936a39ef2799cdd1
eestelle <yosiky@list.ru>
2022-09-19 20:45:54 +0300

    day02: ex00: fixed

--- day02/ex00/first_class.py
+++ day02/ex00/first_class.py
@@ -1,23 +1,10 @@
 
 class Must_read:
-    _file_name = None
-    _fd_file = None
-    _file_lines = None
-
-    def __init__(self, file_name=None):
-        self._file_name = file_name
-        self._fd_file = open(file_name, 'r')
-
-    def __del__(self):
-        self._fd_file.close()
-
-    def printlines(self):
-        self._file_lines = self._fd_file.readlines()
-        for i in self._file_lines:
+    with open('data.csv', 'r') as file:
+        for i in file.readlines():
             print(i, end='')
 
 if __name__ == '__main__':
-    a = Must_read('data.csv')
-    a.printlines()
+    Must_read
 
 
