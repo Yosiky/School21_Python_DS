c435f60594012167b8a08cf0b870839f03af90c3
eestelle <yosiky@list.ru>
2022-09-19 20:50:36 +0300

    day02: ex01: fixed

--- day02/ex01/first_method.py
+++ day02/ex01/first_method.py
@@ -1,23 +1,12 @@
 
 class Research:
-    _file_name = None
-    _fd_file = None
-    _file_lines = None
+    def file_reader():
+        with open('data.csv', 'r') as file:
+            for i in file.readlines():
+                print(i, end='')
 
-    def __init__(self, file_name=None):
-        self._file_name = file_name
-        self._fd_file = open(file_name, 'r')
-
-    def __del__(self):
-        self._fd_file.close()
-
-    def file_reader(self):
-        self._file_lines = self._fd_file.readlines()
-        return self._file_lines
 
 if __name__ == '__main__':
-    a = Research('data.csv')
-    lines = a.file_reader()
-    for i in lines:
-        print(i, end='')
+    a = Research
+    a.file_reader()
 
