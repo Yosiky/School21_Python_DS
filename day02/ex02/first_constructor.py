1b3fe582d983495ab84baab68b99f08949a5e440
eestelle <yosiky@list.ru>
2022-09-19 21:19:51 +0300

    day02: ex02: finish

--- day02/ex02/first_constructor.py
+++ day02/ex02/first_constructor.py
@@ -7,6 +7,12 @@ class Research:
     def file_reader(self):
         with open(self._file_name, 'r') as file:
             lines = file.readlines()
+            if len(lines[0].split(',')) == 2:
+                for i in lines[1:]:
+                    if i[0:3] != '0,1' and i[0:3] != '1,0':
+                        raise Exception("Not valid line in file")
+            else:
+                raise Exception("Not valid header in file")
             for i in lines:
                 print(i, end='')
         return lines
